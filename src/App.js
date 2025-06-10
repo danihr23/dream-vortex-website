4ec257b48bcc4579f759e2bd3da3542f1d1391ac
danihr23 <danailhristov23@gmail.com>
2025-06-10 16:30:35 +0300

    redirect to barcelona - done

--- src/App.js
+++ src/App.js
@@ -1,84 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import React, { useCallback, useState, useEffect } from 'react';
-import styled from 'styled-components/macro';
-import IntroScreen from './components/IntroScreen';
-import CanvasChairScroll from './components/CanvasChairScroll';
-import About from './components/About';
-import Projects from './components/Projects';
-import Team from './components/Team';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
-import Cookies from './components/Cookies';
-import Toolbar from './components/Toolbar';
-import useOrientation from './hooks/useOrientation';
-import ChairVideoComponent from './components/ChairVideoComponent';
-import CanvasChairScrollMobile from './components/CanvasChairScrollMobile';
-
+import Home from './components/Home';
 
 
 function App() {
-  const isPortrait = useOrientation();
-
-  const [isLoading, setIsLoading] = useState(true)
-  const [isAgree, setIsAgree] = useState(false);
-  const [isFooterPriveceClicked, setIsFooterPriveceClicked] = useState(false);
-  const [isToolbarVisible, setIsToolbarVisible] = useState(false);
-  const [onScroll, setOnScroll] = useState(false);
-
-
-  const onPrivacyClick = useCallback(() => {
-    setIsAgree(false)
-    setIsFooterPriveceClicked(true)
-  }, [])
-  const handleScroll = () => {
-    setOnScroll(true)
-  };
-
-  useEffect(() => {
-    const fakeDataFetch = () => {
-      setTimeout(() => {
-        setIsLoading(false)
-      }, 3800)
-    }
-
-    fakeDataFetch();
-    window.addEventListener('scroll', handleScroll);
-    
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [])
-
-
-
   return (
-    <Wrapper >
-      {isLoading && (
-        <IntroScreen />
-      )}
-      <ContentWrapper isLoading={isLoading}>
-        { isToolbarVisible && <Toolbar />}
-        <ChairVideoComponent onScrollProp={onScroll}/>  
-        { isPortrait ? <CanvasChairScrollMobile setIsToolbarVisible={setIsToolbarVisible} /> : <CanvasChairScroll setIsToolbarVisible={setIsToolbarVisible} />}   
-        <About />
-        { isToolbarVisible &&   <Cookies setIsAgree={setIsAgree} isAgree={isAgree} isFooterPriveceClicked={isFooterPriveceClicked} />}
-        {/* <Projects />
-         <Team /> */}
-        <ContactForm />
-        <Footer onClickPrivacy={onPrivacyClick} />
-      </ContentWrapper>
-    </Wrapper>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} >   
+          {/* External redirect route */}
+        <Route
+            path="/barcelona"
+           component ={()=>{
+            window.location.href = 'https://barcelona.dreamvortex.com';
+            return null;
+           }}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-
-const Wrapper = styled.div`
-  height: 100%;
-`;
-
-const ContentWrapper = styled.div`
-  display: ${props => props.isLoading && 'none'};
-`;
-
---
+++ src/components/Home.js
@@ -0,0 +1,84 @@
+
+import React, { useCallback, useState, useEffect } from 'react';
+import styled from 'styled-components/macro';
+import IntroScreen from './IntroScreen';
+import CanvasChairScroll from './CanvasChairScroll';
+import About from './About';
+import Projects from './Projects';
+import Team from './Team';
+import ContactForm from './ContactForm';
+import Footer from './Footer';
+import Cookies from './Cookies';
+import Toolbar from './Toolbar';
+import useOrientation from '../hooks/useOrientation';
+import ChairVideoComponent from './ChairVideoComponent';
+import CanvasChairScrollMobile from './CanvasChairScrollMobile';
+
+
+
+function Home() {
+  const isPortrait = useOrientation();
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [isAgree, setIsAgree] = useState(false);
+  const [isFooterPriveceClicked, setIsFooterPriveceClicked] = useState(false);
+  const [isToolbarVisible, setIsToolbarVisible] = useState(false);
+  const [onScroll, setOnScroll] = useState(false);
+
+
+  const onPrivacyClick = useCallback(() => {
+    setIsAgree(false)
+    setIsFooterPriveceClicked(true)
+  }, [])
+  const handleScroll = () => {
+    setOnScroll(true)
+  };
+
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false)
+      }, 3800)
+    }
+
+    fakeDataFetch();
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [])
+
+
+
+  return (
+    <Wrapper >
+      {isLoading && (
+        <IntroScreen />
+      )}
+      <ContentWrapper isLoading={isLoading}>
+        { isToolbarVisible && <Toolbar />}
+        <ChairVideoComponent onScrollProp={onScroll}/>  
+        { isPortrait ? <CanvasChairScrollMobile setIsToolbarVisible={setIsToolbarVisible} /> : <CanvasChairScroll setIsToolbarVisible={setIsToolbarVisible} />}   
+        <About />
+        { isToolbarVisible &&   <Cookies setIsAgree={setIsAgree} isAgree={isAgree} isFooterPriveceClicked={isFooterPriveceClicked} />}
+        {/* <Projects />
+         <Team /> */}
+        <ContactForm />
+        <Footer onClickPrivacy={onPrivacyClick} />
+      </ContentWrapper>
+    </Wrapper>
+  );
+}
+
+export default Home;
+
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const ContentWrapper = styled.div`
+  display: ${props => props.isLoading && 'none'};
+`;
+
